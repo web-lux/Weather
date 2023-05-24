1b5a92167a364bac076be9fe1cebd1fc8ea62ac8
web-lux <web-lux@outlook.fr>
2023-05-24 09:29:14 +0200

    fix: Import de WeatherModule dans AppModule

--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { HttpClientModule } from "@angular/common/http";
-
 import { AppRoutingModule } from "../app-routing.module";
 import { AppComponent } from "./main/components/main/app.component";
 import { NotFoundComponent } from "./main/components/not-found/not-found.component";
@@ -9,6 +8,7 @@ import { HeaderComponent } from "./main/components/header/header.component";
 import { FormComponent } from "./main/components/form/form.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { WeatherApiService } from "../services/weather-api.service";
+import { WeatherModule } from "./weather/weather.module";
 
 @NgModule({
 	declarations: [
@@ -22,6 +22,7 @@ import { WeatherApiService } from "../services/weather-api.service";
 		HttpClientModule,
 		ReactiveFormsModule,
 		AppRoutingModule,
+		WeatherModule,
 	],
 	providers: [WeatherApiService],
 	bootstrap: [AppComponent],
